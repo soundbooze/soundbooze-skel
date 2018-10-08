4b5c99751531edad1201de3472fef2f78fd3e6e7
bruno/br <soundbooze@gmail.com>
2018-10-08 09:46:22 +0700

    Create proto.js

---
+++ js/proto.js
@@ -0,0 +1,28 @@
+/*
+
+o   o o-o   o   o o  o 
+|   | |  \  |   | | /  
+|   | |   O |   | OO   
+|   | |  /  |   | | \  
+ o-o  o-o    o-o  o  o  Uduk Bla v1.0 - Free as an AIR LICENSE
+ 
+*/
+
+var UdukTempo = (function () {
+
+  var _rBuff = [];
+
+  var reset = function() {
+
+  };
+    
+  var detect = function () {
+    
+  };
+
+  return {
+    reset  : reset,
+    detect : detect
+  };
+
+})();
