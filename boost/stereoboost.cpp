4981ffa4f1261f5761832ef7f222e29037ffe15e
bruno/br <soundbooze@gmail.com>
2020-03-29 23:17:50 +0700

    Add files via upload

--- boost/stereoboost.cpp
+++ boost/stereoboost.cpp
@@ -6,14 +6,21 @@ class Stereoboost : public Plugin<Stereoboost> {
   public:
 
     Stereoboost(double rate)
-      : Plugin<Stereoboost>(4) {
+      : Plugin<Stereoboost>(5) {
       }
 
     void run(uint32_t nframes) {
+
+      float boost = *p(0);
+
+      boost = boost < 1 ? 1 : boost;
+      boost = boost > 20 ? 20 : boost;
+
       for (uint32_t i = 0; i < nframes; ++i) {
-        p(2)[i] = p(0)[i] * 20.9;
-        p(3)[i] = p(1)[i] * 20.9;
+        p(3)[i] = p(1)[i] * boost;
+        p(4)[i] = p(2)[i] * boost;
       }
+
     }
 
 };
